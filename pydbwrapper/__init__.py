f12aecb3e355ed72145c1be4c5988f9c93494c07
martez81 <marcin.kossakowski@gmail.com>
2012-10-26 01:50:03 +0000

    updated setup.py

--- pydbwrapper/__init__.py
+++ pydbwrapper/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 
-""" pyDbWrapper
-"""
-
-__version__ = '0.1'
+__version_info__ = ('0', '1', '0')
+__version__ = '.'.join(__version_info__)
--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 
+from pydbwrapper import __version__
 from distutils.core import setup
 
 setup(
     name='pyDbWrapper',
-    version='0.1.0',
+    version=__version__,
     author='Marcin Kossakowski',
     author_email='marcin.kossakowski@gmail.com',
     packages=['pydbwrapper'],
     url='git://github.com/martez81/pyDbWrapper.git',
     license='LICENSE.txt',
     description='MySQL python driver wrapper',
-    long_description=open('README.txt').read()
+    long_description=open('README.txt').read(),
+    install_requires = ["MySQL-python >= 1.2.2"]
 )
