8d3db1299f128eec272467fbd79bd5d90d567e60
martez81 <marcin.kossakowski@gmail.com>
2012-10-26 00:00:39 +0000

    adding some more files

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+
+setup(
+    name='pyDbWrapper',
+    version='0.1.0',
+    author='Marcin Kossakowski',
+    author_email='marcin.kossakowski@gmail.com',
+    packages=['pyDbWrapper'],
+    url='git://github.com/martez81/pyDbWrapper.git',
+    license='LICENSE.txt',
+    description='MySQL python driver wrapper',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "MySQLdb >= 1.0"
+    ],
+)
